8c9af4a67452be2503e496dc665197649b4cbbc6
Merzlyakov Aleksey <alllll@yandex.ru>
2020-05-22 23:27:25 +0500

    Hometask3 1,2,3 (4-2)

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -26,7 +26,7 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             }else {
                 status = status.nextStatus()
-                "Это неправильный ответ!\n${question.question}" to status.color
+                "Это неправильный ответ\n${question.question}" to status.color
             }
         }
     }
