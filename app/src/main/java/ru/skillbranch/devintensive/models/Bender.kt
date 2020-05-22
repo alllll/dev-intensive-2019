73dfbaa424e8de73b4d9704b26ab0716e1835bec
Merzlyakov Aleksey <alllll@yandex.ru>
2020-05-22 22:55:15 +0500

    Hometask3 1,2,3 (4-1)

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -15,7 +15,7 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
     fun listenAnswer(answer:String) : Pair<String, Triple<Int,Int, Int>>{
         return if(question.answers.contains(answer)){
            question = question.nextQuestion()
-            "Отлично - это правильный ответ!\n${question.question}" to status.color
+            "Отлично - ты справился!\n${question.question}" to status.color
         }
         else {
             countNegotiveAnswer++
@@ -26,7 +26,7 @@ class Bender(var status:Status = Status.NORMAL, var question:Question = Question
                 "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
             }else {
                 status = status.nextStatus()
-                "Это не правильный ответ!\n${question.question}" to status.color
+                "Это неправильный ответ!\n${question.question}" to status.color
             }
         }
     }
